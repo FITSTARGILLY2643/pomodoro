a6053ed56a7d4a2c365333236c227c9cc414694d
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-17 00:20:59 +0300

    Add login and registration forms

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
+from app .models import User
 
 app= create_app('development')
 # app= create_app('production')
@@ -12,8 +13,9 @@ manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
-
-
+@manager.shell
+def make_shell():
+    return dict(db=db,app=app,User=User)
 
 if __name__ == '__main__':
     manager.run()
